473832b139c97d5aa349ff3636087631cadb5999
Jagriti <jagriti.agni@gmail.com>
2020-10-06 02:28:23 +0530

    [Jagriti]ADDED: welcome message

---
+++ src/main/java/com/bl/junitMood/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.bl.junitMood.HotelReservationSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the hotel reservation system." );
+    }
+}
