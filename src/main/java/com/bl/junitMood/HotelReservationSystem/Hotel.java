aa1fd5ae4057d6bc041baf04e641db0b846f9a0c
Jagriti <jagriti.agni@gmail.com>
2020-10-06 16:26:01 +0530

    [Jagriti]FIXED: error, files not showing in local repo

---
+++ src/main/java/com/bl/junitMood/HotelReservationSystem/Hotel.java
@@ -0,0 +1,12 @@
+package com.bl.junitMood.HotelReservationSystem;
+
+public class Hotel {
+	
+	private String hotelName;
+	private int regularDailyRate;
+	
+	Hotel(String hotelName, int regularDailyRate) {
+		this.hotelName = hotelName;
+		this.regularDailyRate = regularDailyRate;
+	}
+}
---
+++ src/main/java/com/bl/junitMood/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,14 @@
+package com.bl.junitMood.HotelReservationSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservation 
+{
+	public boolean printWelcome() {
+		 System.out.println( "Welcome to the hotel reservation system." );
+		 return true;
+	}
+	
+}
